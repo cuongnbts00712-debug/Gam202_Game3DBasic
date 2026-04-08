2f8f6f8c3a3d00a4e577d3eee893be629f0e88c9
cuongnbts00712-debug <cuongnbts00712@fpt.edu.vn>
2026-04-08 23:36:42 +0700

    Update RunState.cs

--- Assets/Script/RunState.cs
+++ Assets/Script/RunState.cs
@@ -31,7 +31,6 @@ public class RunState : StateMachineBehaviour
         // Thiết lập tốc độ chạy cho enemy khi vào trạng thái Run
         agent.speed = 5f;
 
-        animator.SetBool("isChasing", false);
     }
 
 
